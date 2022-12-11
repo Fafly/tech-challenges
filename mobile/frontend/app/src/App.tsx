43c732b01f166bd580797f17822709b4af49277b
Fafly <louislebeke@gmail.com>
2022-12-11 18:26:03 +0100

    :sparkles: Add survey screen.

--- mobile/frontend/app/src/App.tsx
+++ mobile/frontend/app/src/App.tsx
@@ -15,7 +15,7 @@ const Stack = createNativeStackNavigator<RootStackParamList>();
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator initialRouteName="Home">
         <Stack.Screen name="Home" component={Home} />
         <Stack.Screen name="Survey" component={Survey} />
       </Stack.Navigator>
